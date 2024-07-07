22b837ef8890d0fc1ff53cca92ac8f2907abe738
pko89403@gmail.com <pko89403@gmail.com>
2024-07-07 21:51:23 +0900

    init project

---
+++ E4SRec/fine-tuning.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+torchrun --nproc_per_node=2 --master_port=1234 finetune.py \
+    --base_model garage-bAInd/Platypus2-70B-instruct \
+    --data_path ML1M \
+    --task_type general \
+    --output_dir ./LLM4Rec \
+    --batch_size 16 \
+    --micro_batch_size 1 \
+    --num_epochs 1 \
+    --learning_rate 0.0003 \
+    --cutoff_len 4096 \
+    --val_set_size 0 \
+    --lora_r 16 \
+    --lora_alpha 16 \
+    --lora_dropout 0.05 \
+    --lora_target_modules '[gate_proj, down_proj, up_proj]' \
+    --train_on_inputs False \
+    --add_eos_token False \
+    --group_by_length False \
+    --prompt_template_name alpaca \
+    --lr_scheduler 'cosine' \
+    --warmup_steps 100
--- Readme.md
+++ Readme.md
@@ -3,3 +3,4 @@ This is shadowing repository of the awesome llm4recsys papers
 
 - [LLamaRec](https://github.com/Yueeeeeeee/LlamaRec)
 - [MacRec](https://github.com/wzf2000/MACRec)
+- [E4SRec]()
